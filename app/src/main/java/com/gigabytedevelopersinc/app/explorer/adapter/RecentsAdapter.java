d3fc5ee4b28c74b5fef8c7a76d5ef27a1d6e5aac
emmanwokoma <nwokomaemmanuel@gmail.com>
2017-08-05 06:11:58 +0100

    Deploying App Resources (Main JAVA Files - All Activities)
    Deploying App Resources (Main JAVA Files - All Activities) are now live!

---
+++ app/src/main/java/com/gigabytedevelopersinc/app/explorer/adapter/RecentsAdapter.java
@@ -0,0 +1,103 @@
+package com.gigabytedevelopersinc.app.explorer.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.gigabytedevelopersinc.app.explorer.R;
+import com.gigabytedevelopersinc.app.explorer.cursor.RootCursorWrapper;
+import com.gigabytedevelopersinc.app.explorer.misc.IconColorUtils;
+import com.gigabytedevelopersinc.app.explorer.misc.IconHelper;
+import com.gigabytedevelopersinc.app.explorer.model.DocumentInfo;
+import com.gigabytedevelopersinc.app.explorer.model.DocumentsContract;
+import com.gigabytedevelopersinc.app.explorer.model.DocumentsContract.Document;
+import com.gigabytedevelopersinc.app.explorer.setting.SettingsActivity;
+
+import static com.gigabytedevelopersinc.app.explorer.model.DocumentInfo.getCursorInt;
+import static com.gigabytedevelopersinc.app.explorer.model.DocumentInfo.getCursorString;
+
+public class RecentsAdapter extends CursorRecyclerViewAdapter<RecentsAdapter.ViewHolder> {
+
+    private final IconHelper mIconHelper;
+    private final int mDefaultColor;
+    private Context mContext;
+    private OnItemClickListener onItemClickListener;
+
+    public RecentsAdapter(Context context, Cursor cursor){
+        super(context, cursor);
+        mContext = context;
+        mIconHelper = new IconHelper(context);
+        mDefaultColor = SettingsActivity.getPrimaryColor();
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
+        viewHolder.setData(cursor);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recent, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        onItemClickListener = listener;
+    }
+
+    public OnItemClickListener getOnItemClickListener(){
+        return onItemClickListener;
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(ViewHolder item, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView iconMime;
+        private final ImageView iconThumb;
+        private final View iconMimeBackground;
+        private Cursor mCursor;
+        public DocumentInfo mDocumentInfo;
+
+        public ViewHolder(View v) {
+            super(v);
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(ViewHolder.this, getLayoutPosition());
+                }
+            });
+            iconMime = (ImageView) v.findViewById(R.id.icon_mime);
+            iconThumb = (ImageView) v.findViewById(R.id.icon_thumb);
+            iconMimeBackground = v.findViewById(R.id.icon_mime_background);
+        }
+
+        public void setData(Cursor cursor){
+            mDocumentInfo = DocumentInfo.fromDirectoryCursor(cursor);
+            final String docAuthority = getCursorString(cursor, RootCursorWrapper.COLUMN_AUTHORITY);
+            final String docId = getCursorString(cursor, Document.COLUMN_DOCUMENT_ID);
+            final String docMimeType = getCursorString(cursor, Document.COLUMN_MIME_TYPE);
+            final String docPath = getCursorString(cursor, Document.COLUMN_PATH);
+            final String docDisplayName = getCursorString(cursor, Document.COLUMN_DISPLAY_NAME);
+            final int docIcon = getCursorInt(cursor, Document.COLUMN_ICON);
+            final int docFlags = getCursorInt(cursor, Document.COLUMN_FLAGS);
+
+            mIconHelper.stopLoading(iconThumb);
+
+            iconMimeBackground.setVisibility(View.VISIBLE);
+            iconMimeBackground.setBackgroundColor(IconColorUtils.loadMimeColor(mContext, docMimeType, docAuthority, docId, mDefaultColor));
+            iconThumb.animate().cancel();
+            iconThumb.setAlpha(0f);
+
+            final Uri uri = DocumentsContract.buildDocumentUri(docAuthority, docId);
+            mIconHelper.loadThumbnail(uri, docPath, docMimeType, docFlags, docIcon, iconMime, iconThumb, iconMimeBackground);
+        }
+    }
+}
---
+++ app/src/main/java/com/gigabytedevelopersinc/app/explorer/adapter/RootsAdapter.java
@@ -0,0 +1,158 @@
+package com.gigabytedevelopersinc.app.explorer.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.AppItem;
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.BookmarkItem;
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.Item;
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.RootComparator;
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.RootItem;
+import com.gigabytedevelopersinc.app.explorer.fragment.RootsFragment.SpacerItem;
+import com.gigabytedevelopersinc.app.explorer.model.RootInfo;
+import com.gigabytedevelopersinc.app.explorer.setting.SettingsActivity;
+
+public class RootsAdapter extends ArrayAdapter<Item> {
+
+    public RootsAdapter(Context context, Collection<RootInfo> roots, Intent includeAppss) {
+        super(context, 0);
+
+        int defaultColor = SettingsActivity.getPrimaryColor(context);
+        RootItem recents = null;
+        RootItem images = null;
+        RootItem videos = null;
+        RootItem audio = null;
+        RootItem downloads = null;
+        RootItem root_root = null;
+        RootItem phone = null;
+
+        final List<RootInfo> clouds = new ArrayList<>();
+        final List<RootInfo> locals = new ArrayList<>();
+        final List<RootInfo> extras = new ArrayList<>();
+        final List<RootInfo> bookmarks = new ArrayList<>();
+
+        for (RootInfo root : roots) {
+            if (root.isRecents()) {
+                recents = new RootItem(root, defaultColor);
+            } else if (root.isBluetoothFolder() || root.isDownloadsFolder() || root.isAppBackupFolder()) {
+                extras.add(root);
+            } else if (root.isBookmarkFolder()) {
+                bookmarks.add(root);
+            } else if (root.isPhoneStorage()) {
+                phone = new RootItem(root, defaultColor);
+            } else if (root.isStorage()) {
+                locals.add(root);
+            } else if (root.isRootedStorage()) {
+                root_root = new RootItem(root, defaultColor);
+            } else if (root.isDownloads()) {
+                downloads = new RootItem(root, defaultColor);
+            } else if (root.isImages()) {
+                images = new RootItem(root, defaultColor);
+            } else if (root.isVideos()) {
+                videos = new RootItem(root, defaultColor);
+            } else if (root.isAudio()) {
+                audio = new RootItem(root, defaultColor);
+            } else {
+                clouds.add(root);
+            }
+        }
+
+        final RootComparator comp = new RootComparator();
+        Collections.sort(clouds, comp);
+        //Collections.sort(locals, comp);
+        //Collections.reverse(locals);
+
+        for (RootInfo local : locals) {
+            add(new RootItem(local, defaultColor));
+        }
+        if (phone != null) add(phone);
+
+        for (RootInfo extra : extras) {
+            add(new RootItem(extra, defaultColor));
+        }
+
+        if (root_root != null) {
+            add(new SpacerItem());
+            add(root_root);
+        }
+
+        if (bookmarks.size() > 0) {
+            add(new SpacerItem());
+            for (RootInfo bookmark : bookmarks) {
+                add(new BookmarkItem(bookmark));
+            }
+        }
+
+        add(new SpacerItem());
+        if (recents != null) add(recents);
+        if (images != null) add(images);
+        if (videos != null) add(videos);
+        if (audio != null) add(audio);
+        if (downloads != null) add(downloads);
+
+        //if (includeApps == null) {
+        add(new SpacerItem());
+        for (RootInfo cloud : clouds) {
+            add(new RootItem(cloud, defaultColor));
+        }
+/*                final PackageManager pm = context.getPackageManager();
+                final List<ResolveInfo> infos = pm.queryIntentActivities(
+                        includeApps, PackageManager.MATCH_DEFAULT_ONLY);
+
+                final List<AppItem> apps = Lists.newArrayList();
+
+                // Omit ourselves from the list
+                for (ResolveInfo info : infos) {
+                    if (!context.getPackageName().equals(info.activityInfo.packageName)) {
+                        apps.add(new AppItem(info));
+                    }
+                }
+
+                if (apps.size() > 0) {
+                    add(new SpacerItem());
+                    for (Item item : apps) {
+                        add(item);
+                    }
+                }*/
+        //}
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Item item = getItem(position);
+        return item.getView(convertView, parent);
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return getItemViewType(position) != 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        final Item item = getItem(position);
+        if (item instanceof RootItem || item instanceof AppItem) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+}
