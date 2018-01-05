2a7a0179e8f7cc5f3d109ee3118263829d8bda7a
Emmanuel Nwokoma <nwokomaemmanuel@gmail.com>
2018-01-05 06:48:29 +0100

    Update FireFiles
    Update FireFiles
    Version 2 release

--- app/src/main/java/com/gigabytedevelopersinc/app/explorer/provider/ExternalStorageProvider.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/explorer/provider/ExternalStorageProvider.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2017 Gigabyte Developers Incorporated
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.gigabytedevelopersinc.app.explorer.provider;
 
 import android.annotation.SuppressLint;
@@ -34,7 +17,7 @@ import android.os.FileObserver;
 import android.os.Handler;
 import android.os.ParcelFileDescriptor;
 import android.support.v4.os.EnvironmentCompat;
-import android.support.v4.provider.DocumentFile;
+import android.support.provider.DocumentFile;
 import android.support.v4.util.ArrayMap;
 import android.text.TextUtils;
 import android.util.Log;
