cec68431c3340b170756ed06754ff5af3bd0c2ef
Emmanuel Nwokoma <nwokomaemmanuel@gmail.com>
2018-01-19 20:42:18 +0100

    Fixed suggestion in https://github.com/gigabytedevelopers/FireFiles/issues/2#issuecomment-357307554 as recommended by @IzzySoft in #2
    Also fixed issue with assemble for fdroid in FireFiles https://gitlab.com/fdroid/rfp/issues/452#note_54275864

--- app/src/main/java/com/gigabytedevelopersinc/app/explorer/StandaloneActivity.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/explorer/StandaloneActivity.java
@@ -85,6 +85,21 @@ import static com.gigabytedevelopersinc.app.explorer.fragment.DirectoryFragment.
 import static com.gigabytedevelopersinc.app.explorer.fragment.DirectoryFragment.ANIM_NONE;
 import static com.gigabytedevelopersinc.app.explorer.fragment.DirectoryFragment.ANIM_SIDE;
 import static com.gigabytedevelopersinc.app.explorer.fragment.DirectoryFragment.ANIM_UP;
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 public class StandaloneActivity extends BaseActivity {
     public static final String TAG = "StandaloneActivity";
