8f46244c9fa4b8adde61b2680164238a1492981c
Emmanuel Nwokoma <nwokomaemmanuel@gmail.com>
2018-02-13 06:48:14 +0100

    Remove FireFiles
    Co-Authored-By: Okocha Ebube <okochaebube@gmail.com>
    Co-Authored-By: Gigabyte Developers Incorporated <gigabytedevelopersinc@users.noreply.github.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/explorer/libcore/io/LongCompat.java
+++
@@ -1,7 +0,0 @@
-package com.gigabytedevelopersinc.app.explorer.libcore.io;
-
-public class LongCompat {
-	public static int compare(long x, long y) {
-		return (x < y) ? -1 : ((x == y) ? 0 : 1);
-	}
-}
